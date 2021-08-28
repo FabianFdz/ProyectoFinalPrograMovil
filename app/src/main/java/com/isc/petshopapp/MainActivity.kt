bce8c379e549d672edfb3b4ccca6f5506cb1182f
yamacoder9421 <ymoran94211@ufide.ac.cr>
2021-08-28 10:04:28 -0600

    cambiosYamill14

--- app/src/main/java/com/isc/petshopapp/MainActivity.kt
+++ app/src/main/java/com/isc/petshopapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.isc.petshopapp
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -10,7 +11,10 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+
 import com.isc.petshopapp.databinding.ActivityMainBinding
+//import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
@@ -49,5 +53,15 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.action_logoff->{
+                //Firebase.auth.signOut
+                finish()
+                true
+            }else -> super.onOptionsItemSelected(item)
+        }
     }
 }
